c95a22fb8143ce31119940225c8ecbdbc03125a4
emagged <ariulashmbgerdo@gmail.com>
2022-03-08 20:19:56 +0700

    Lesson 1 HW.

---
+++ userapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import UserModel
+
+class UserModelSerializer(ModelSerializer):
+    class Meta: 
+        model = UserModel
+        fields = ['username', 'first_name', 'last_name', 'email']
