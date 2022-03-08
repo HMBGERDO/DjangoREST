d8de37f01dda009109c341a603810ccaaffb230f
emagged <ariulashmbgerdo@gmail.com>
2022-03-08 20:27:43 +0700

    Lesson 1. Secret key hidden now

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from temp import info
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8#%7b*j)e18yqmzd%t6829-32mq2l-n0_hvp=j2!vn9&8z0t12'
+SECRET_KEY = info.django_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
