30952cd510733466c82193aae4225c62e421163d
Kras <ariulashmbgerdo@gmail.com>
2022-05-08 09:50:57 +0000

    lesson09 hw

--- userapp/views.py
+++ userapp/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics, mixins
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
-from rest_framework.response import Response
 from .models import UserModel
-from .serializers import UserModelSerializer
+from .serializers import UserModelSerializer, UserModelSerializer_v1
+from django.http.response import Http404
 
-class UserModelViewSet(viewsets.ViewSet):
+class UserModelAPIView(generics.CreateAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin):
     renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = UserModel.objects.all()
 
-    def list(self, request):
-        users = UserModel.objects.all()
-        serializer = UserModelSerializer(users, many=True)
-        return Response(serializer.data)
+    def get(self, request, **kwargs):
+        if kwargs.get('pk'):
+            return self.retrieve(request)
+        else:
+            return self.list(request)
 
-    def retrieve(self, request, pk=None):
-        user = get_object_or_404(UserModel, pk=pk)
-        serializer = UserModelSerializer(user)
-        return Response(serializer.data)
+    def get_serializer_class(self):
+        if self.request.version == 'v1':
+            return UserModelSerializer_v1
+        elif self.request.version == 'v0':
+            return UserModelSerializer
+        else:
+            raise Http404
     
